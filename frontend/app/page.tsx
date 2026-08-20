c36dc03d20bed762a256e865212df0b70dbd3a06
Atharv Remeshan <atharv.rem@gmail.com>
2026-08-20 07:02:15 +0530

    hero section done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,69 +1,63 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-row border-black">
+      <div className="flex flex-col items-center w-2xl min-h-screen border-r-2 border-[#efefef] border-l-2">
+        <div className="flex flex-row items-center justify-left w-full p-5">
+          <Image
+            src="./logo.svg"
+            alt="Opacity Logo"
+            width={30}
+            height={30}
+          />
+          <span className="font-pixel text-[26px] ml-2 text-[#1d1d1d]">opacity</span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="flex flex-col items-start justify-center w-full pl-[20px] pr-[15px] mt-[50px]">
+          <span className="font-pixel text-[40px] text-[#1d1d1d]">Helping Every</span>
+          <span className="font-pixel text-[40px] text-[#1d1d1d] leading-[40px]">Dyslexic Mind Thrive</span>
+          <div className="font-open-sauce text-[20px] text-[#5e5e5e] mt-[20px] leading-[28px]">
+            A gamified{" "}
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src="./game.svg"
+              alt="Gamification Icon"
+              width={25}
+              height={25}
+              className="inline-block align-middle"
+            />{" "}
+            platform that helps identify potential learning difficulties early and
+            turns gameplay into personalized {" "}
+            <Image
+              src="./face.svg"
+              alt="face Icon"
+              width={22}
+              height={22}
+              className="inline-block align-middle mb-[5px]"
+            />{" "}
+            learning activities for every child.
+            <Image
+              src="./baby.svg"
+              alt="Baby Icon"
+              width={26}
+              height={26}
+              className="inline-block align-middle"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <button>
+            <div className="button-shadow flex flex-row items-center justify-center w-fit h-fit bg-[#1b1b1b] shover:bg-[#323232]  hover:translate-y-[-5px] transition-all duration-200 rounded-[20px] mt-[30px] px-[17px] py-[8px]">
+              <Image
+                src="./game2.svg"
+                alt="Play Icon"
+                width={25}
+                height={25}
+                className="mr-[10px]"
+              />
+              <span className="font-pixel text-[20px] text-[#ffffff]">lets play</span>
+            </div>
+          </button>
         </div>
-      </main>
-    </div>
+        <div className="dashed-line w-full mt-[50px]"></div>
+      </div>
+      
+      </div>
   );
 }
